e09a724256c308e03207bbb75dfbd1e6484bdab3
banterle <frabante@gmail.com>
2014-12-18 17:37:57 +0100

    updated to new operators

--- samples/opengl_operators/main.cpp
+++ samples/opengl_operators/main.cpp
@@ -79,7 +79,6 @@ public:
         glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
 
-
         //simple tone mapping: gamma + exposure correction
         imgOutTMO = tmo->Process(SingleGL(&img), imgOutTMO);
 
