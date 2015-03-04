9ebd23ef43ec9a5508614d959a2c9912e28666b9
Francesco Banterle <frabante@gmail.com>
2015-03-04 15:53:51 +0100

    removed a debugging flag

--- samples/opengl_tone_mapping/main.cpp
+++ samples/opengl_tone_mapping/main.cpp
@@ -26,8 +26,6 @@ file, You can obtain one at http://mozilla.org/MPL/2.0/.
     #include "../opengl_common_code/gl_core_4_0.h"
 #endif
 
-#define PIC_DEBUG
-
 #include "piccante.hpp"
 
 #include <QKeyEvent>
