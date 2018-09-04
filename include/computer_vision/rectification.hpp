73696221a53690477cf0a2465ceaeb0ac426f0ca
Francesco Banterle <frabante@gmail.com>
2018-09-04 15:35:31 +0200

    fix

--- include/computer_vision/rectification.hpp
+++ include/computer_vision/rectification.hpp
@@ -140,6 +140,48 @@ PIC_INLINE ImageVec *computeImageRectification(Image *img0,
     return out;
 }
 
+/**
+ * @brief computeImageRectification
+ * @param img0
+ * @param img1
+ * @param K0
+ * @param R0
+ * @param t0
+ * @param K1
+ * @param R1
+ * @param t1
+ * @param out
+ * @return
+ */
+PIC_INLINE ImageVec *computeImageRectification(Image *img0,
+                                               Image *img1,
+                                               Eigen::Matrix3d &K0,
+                                               Eigen::Matrix3d &R0,
+                                               Eigen::Vector3d &t0,
+                                               Eigen::Matrix3d &K1,
+                                               Eigen::Matrix3d &R1,
+                                               Eigen::Vector3d &t1,
+                                               ImageVec *out = NULL)
+{
+    //NOTE: we should check that img0 and img1 are valid...
+    if(img0 == NULL || img1 == NULL) {
+        return out;
+    }
+
+    if(out == NULL) {
+        out = new ImageVec();
+    }
+
+    Eigen::Matrix34d M0_r, M1_r;
+    Eigen::Matrix3d T0, T1;
+
+    pic::cameraRectify(K0, R0, t0, K1, R1, t1, M0_r, M1_r, T0, T1);
+
+    out = computeImageRectificationWarp(img0, img1, T0, T1, out);
+
+    return out;
+}
+
 #endif // PIC_DISABLE_EIGEN
 
 } // end namespace pic
