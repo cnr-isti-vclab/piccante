4f01cdfd5ab132c08b14be08eeb2d58d2d595922
Francesco Banterle <frabante@gmail.com>
2018-11-16 08:48:26 -0800

    Update include/gl/filtering/filter_warp_2d.hpp

--- include/gl/filtering/filter_warp_2d.hpp
+++ include/gl/filtering/filter_warp_2d.hpp
@@ -145,7 +145,7 @@ PIC_INLINE void FilterGLWarp2D::update(Matrix3x3 h, bool bSameSize = false, bool
     this->bCentroid = bCentroid;
 
     this->h = h;
-    h.getInverse(&h_inv);
+    h.inverse(&h_inv);
 
     technique.bind();
     technique.setUniform3x3("h_inv", h_inv.data, true);
