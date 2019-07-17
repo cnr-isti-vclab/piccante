f448d676981f0705110a7613165f1c3286b2af17
Francesco Banterle <frabante@gmail.com>
2019-07-17 16:06:51 +0200

    Update filter_channel.hpp

--- include/gl/filtering/filter_channel.hpp
+++ include/gl/filtering/filter_channel.hpp
@@ -19,7 +19,7 @@ file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #define PIC_GL_FILTERING_FILTER_CHANNEL_HPP
 
 #include "../../base.hpp"
-
+#include "../../util/math.hpp"
 #include "../../gl/filtering/filter.hpp"
 
 namespace pic {
@@ -33,7 +33,26 @@ protected:
     /**
      * @brief initShaders
      */
-    void initShaders();
+    void initShaders()
+    {
+        fragment_source = MAKE_STRING
+                          (
+        uniform sampler2D u_tex; \n
+        uniform int channel; \n
+        out     vec4 f_color; \n
+        \n
+        void main(void) {
+            \n
+            ivec2 coords = ivec2(gl_FragCoord.xy); \n
+            vec4 color = texelFetch(u_tex, coords, 0); \n
+            float v = color[channel]; \n
+            f_color = vec4(v, v, v, 1.0); \n
+
+        }
+                          );
+
+        technique.initStandard("330", vertex_source, fragment_source, "FilterGLChannel");
+    }
 
     int channel;
 
@@ -43,13 +62,27 @@ public:
      * @brief FilterGLChannel
      * @param channel
      */
-    FilterGLChannel(int channel);
+    FilterGLChannel(int channel) : FilterGL()
+    {
+        initShaders();
+        update(channel);
+    }
 
     /**
      * @brief update
      * @param channel
      */
-    void update(int channel);
+    void update(int channel)
+    {
+        this->channel = CLAMP(channel, 4);
+
+        if(technique.isValid()) {
+            technique.bind();
+            technique.setUniform1i("u_tex", 0);
+            technique.setUniform1i("channel", channel);
+            technique.unbind();
+        }
+    }
 
     /**
      * @brief OutputSize
@@ -105,52 +138,13 @@ public:
         ImageGL *outB = filter.Process(SingleGL(&imgIn), NULL);
 
         outR->loadToMemory();
-        outR->Write("channel_R.pfm");
+        outR->Write("channel_R.bmp");
         outG->loadToMemory();
-        outG->Write("channel_G.pfm");
+        outG->Write("channel_G.bmp");
         outB->loadToMemory();
-        outB->Write("channel_B.pfm");
+        outB->Write("channel_B.bmp");
     }
 };
-
-PIC_INLINE FilterGLChannel::FilterGLChannel(int channel) : FilterGL()
-{
-    initShaders();
-    update(channel);
-}
-
-PIC_INLINE void FilterGLChannel::initShaders()
-{
-    fragment_source = MAKE_STRING
-                      (
-    uniform sampler2D u_tex; \n
-    uniform int channel; \n
-    out     vec4 f_color; \n
-    \n
-    void main(void) {
-        \n
-        ivec2 coords = ivec2(gl_FragCoord.xy); \n
-        vec3 color = texelFetch(u_tex, coords, 0).xyz; \n
-        float v = color[channel]; \n
-        f_color = vec4(v, v, v, 1.0); \n
-
-    }
-                      );
-
-    technique.initStandard("330", vertex_source, fragment_source, "FilterGLChannel");
-}
-
-PIC_INLINE void FilterGLChannel::update(int channel)
-{
-    this->channel = channel > -1 ? channel : 0;
-
-    if(technique.isValid()) {
-        technique.bind();
-        technique.setUniform1i("u_tex", 0);
-        technique.setUniform1i("channel", channel);
-        technique.unbind();
-    }
-}
 
 } // end namespace pic
 
