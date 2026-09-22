f80b6aec7f32917f9dc76f6ec469dc7f990d86bd
Francesco Banterle <frabante@gmail.com>
2026-09-22 16:42:56 +0200

    Update rasterizer.hpp

--- include/util/rasterizer.hpp
+++ include/util/rasterizer.hpp
@@ -26,10 +26,12 @@ file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #include "../util/vec.hpp"
 #include "../util/math.hpp"
 
-#ifndef PIC_EIGEN_NOT_BUNDLED
-    #include "../externals/Eigen/Dense"
-#else
-    #include <Eigen/Dense>
+#ifndef PIC_DISABLE_EIGEN
+    #ifndef PIC_EIGEN_NOT_BUNDLED
+        #include "../externals/Eigen/Dense"
+    #else
+        #include <Eigen/Dense>
+    #endif
 #endif
 
 namespace pic {
